5429c4550b999d19114a07386ea1930f33f5cc2c
Scott <scottbrussell@gmail.com>
2015-02-11 12:54:52 -0500

    >>

---
+++ TheBookStore/Infrastructure/EnforceHttpsAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace TheBookStore.Infrastructure
+{
+    public class EnforceHttpsAttribute : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            if (actionContext.Request.RequestUri.Scheme != Uri.UriSchemeHttps)
+            {
+                actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden)
+                {
+                    ReasonPhrase = "HTTPS Required"
+                };
+            }
+            else
+            {
+                base.OnAuthorization(actionContext);
+            }
+        }
+    }
+}
