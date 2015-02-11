bd5cf4316a82df64b706639ec50cd67fb6b96093
Scott <scottbrussell@gmail.com>
2015-02-11 12:53:50 -0500

    Finished enabling HTTPS/SSL

--- TheBookStore/App_Start/WebApiConfig.cs
+++ TheBookStore/App_Start/WebApiConfig.cs
@@ -21,6 +21,8 @@ namespace TheBookStore
                 defaults: new { id = RouteParameter.Optional }
             );
 
+            config.Filters.Add(new EnforceHttpsAttribute());
+
             config.MessageHandlers.Add(new BasicAuthenticationHandler(new CustomPrincipalProvider()));
 
             config.Filters.Add(new System.Web.Http.AuthorizeAttribute());
